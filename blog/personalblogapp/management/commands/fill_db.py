8e39c347c3f95b32016f19da6ecced751589c9dd
Radif <i@radif.ru>
2021-07-23 12:23:21 +0300

    fixed specific bugs for postgresql

--- blog/personalblogapp/management/commands/fill_db.py
+++ blog/personalblogapp/management/commands/fill_db.py
@@ -3,6 +3,8 @@ import json
 
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from django.core.management.color import no_style
+from django.db import connection
 
 from blog.settings import JSON_PATH
 from personalblogapp.models import UserPost
@@ -22,33 +24,42 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        if not get_user_model().objects.filter(username='radif').exists():
+        if not get_user_model().objects.exists():
             get_user_model().objects.create_superuser(
                 username='radif',
                 email='mail@radif.ru',
                 password='qwertytrewq')
 
-        if not get_user_model().objects.filter(username='Kolya').exists():
             get_user_model().objects.create_user(
                 username='Kolya',
                 email='kolya@blog.local',
                 password='qwertytrewq')
 
-        if not get_user_model().objects.filter(username='Alyosha').exists():
             get_user_model().objects.create_user(
                 username='Alyosha',
                 email='alyosha@blog.local',
                 password='qwertytrewq')
 
-        os.system('python manage.py makemigrations personalblogapp')
-        os.system('python manage.py migrate --run-syncdb')
+            os.system('python manage.py makemigrations')
+            os.system('python manage.py migrate --run-syncdb')
 
+        # создание постов с привязкой к конкретным пользователям,
+        # которые были созданы выше, по внешним ключам
         posts = load_from_json('personalblogapp_userpost')
-        UserPost.objects.all().delete()
-        for post in posts:
-            user_name = post['fields']['user']
-            _user = get_user_model().objects.get(id=user_name)
-            post['fields']['user'] = _user
+        if not UserPost.objects.exists():
+            UserPost.objects.all().delete()
+            for post in posts:
+                user_name = post['fields']['user']
+                _user = get_user_model().objects.get(id=user_name)
+                post['fields']['user'] = _user
 
-            new_publication = UserPost(**{'id': post['pk']}, **post['fields'])
-            new_publication.save()
+                new_publication = UserPost(**{'id': post['pk']},
+                                           **post['fields'])
+                new_publication.save()
+
+        # сброс последовательностей в базе данных
+        sequence_sql = connection.ops.sequence_reset_sql(
+            no_style(), [UserPost, get_user_model()])
+        with connection.cursor() as cursor:
+            for sql in sequence_sql:
+                cursor.execute(sql)
