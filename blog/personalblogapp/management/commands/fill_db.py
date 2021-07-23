993644c240d06dd75e248523d505168952291240
Radif <i@radif.ru>
2021-07-23 13:22:08 +0300

    fixed specific bugs for postgresql

--- blog/personalblogapp/management/commands/fill_db.py
+++ blog/personalblogapp/management/commands/fill_db.py
@@ -63,7 +63,7 @@ class Command(BaseCommand):
                                            **post['fields'])
                 new_publication.save()
 
-                self.syncdb()
+                self.reset_sequences()
 
     @staticmethod
     def syncdb():
